837bb2fb2f6c31251737b2561dedfe352bc14e11
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-02-16 14:46:19 +0000

    added user login route

---
+++ backend/controllers/userController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+// Auth  user and fethc token
+// GET /api/users/login
+// Public
+
+const authUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: null //generateToken(user._id)
+		});
+	} else {
+		res.status(401);
+		throw new Error('Invalid email or password');
+	}
+});
+
+export { authUser };
