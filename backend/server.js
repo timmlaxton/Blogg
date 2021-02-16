04ca32156bceecd09dfb3d1dc4b1ca2fea04b829
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-02-16 07:41:53 +0000

    connected mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import blogs from './data/blogs.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
